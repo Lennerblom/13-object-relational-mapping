e8b2be1bbb8c0903c5618bb443c76adcf948bd27
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-06-24 18:29:21 -0700

    changed db to heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 require('babel-register');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_PINK_URI)
   .catch(err => console.error(err));
 
 require('./src/app.js').start(process.env.PORT);
